be9a938ab4bc12c7b63431914b1a58fb82dd26de
Hila Shoshan <hilashoshan0605@gmail.com>
2020-04-06 20:12:46 +0300

    finished first 3 subtasks

--- ex1_utils.py
+++ ex1_utils.py
@@ -1,57 +1,61 @@
-"""
-        '########:'##::::'##::::'##:::
-         ##.....::. ##::'##:::'####:::
-         ##::::::::. ##'##::::.. ##:::
-         ######:::::. ###::::::: ##:::
-         ##...:::::: ## ##:::::: ##:::
-         ##:::::::: ##:. ##::::: ##:::
-         ########: ##:::. ##::'######:
-        ........::..:::::..:::......::
-"""
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
 
 def myID() -> np.int:
-    """
-    Return my ID (not the friend's ID I copied from)
-    :return: int
-    """
-    return 123456789
+    return 207931106
 
 
-def imReadAndConvert(filename: str, representation: int) -> np.ndarray:
-    """
-    Reads an image, and returns and returns in converted as requested
+""" 4.1
+    Reads an image, and returns in converted as requested
     :param filename: The path to the image
     :param representation: GRAY_SCALE or RGB
     :return: The image object
-    """
+"""
+def imReadAndConvert(filename: str, representation: int) -> np.ndarray:
+    if(representation == 1): #Gray_Scale representation
+        img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    else: # =2, RGB
+        img = cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2RGB)
+    return img
     pass
 
 
-def imDisplay(filename: str, representation: int):
-    """
+""" 4.2
     Reads an image as RGB or GRAY_SCALE and displays it
     :param filename: The path to the image
     :param representation: GRAY_SCALE or RGB
     :return: None
-    """
+"""
+def imDisplay(filename: str, representation: int):
+    img = imReadAndConvert(filename, representation)
+    plt.imshow(img)
+    plt.show()
+    return None
     pass
 
 
-def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
-    """
+""" 4.3 (1)
     Converts an RGB image to YIQ color space
     :param imgRGB: An Image in RGB
     :return: A YIQ in image color space
-    """
+"""
+def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
+    transform = np.ndarray([[0.299, 0.587, 0.114], [0.596, -0.275, -0.321], [0.212, -0.523, 0.311]])
+    return transform.dot(imgRGB)
     pass
 
 
-def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
-    """
+""" 4.3 (2)
     Converts an YIQ image to RGB color space
     :param imgYIQ: An Image in YIQ
     :return: A RGB in image color space
-    """
+"""
+def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
+    transform = np.ndarray([[0.299, 0.587, 0.114], [0.596, -0.275, -0.321], [0.212, -0.523, 0.311]])
+    mat = transform.transpose()
+    return mat.dot(imgYIQ)
     pass
 
 
---
+++ test.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+import ex1_utils
+
+def main():
+    ex1_utils.imDisplay('beach.jpg', 1)
+
+#if __name__ == '__main__':
+#    main()
