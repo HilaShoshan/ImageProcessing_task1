48a894d52e7edc62ef4f55e92262f9bcd7576ae6
Hila Shoshan <hilashoshan0605@gmail.com>
2020-04-23 17:54:27 +0300

    finished gammaDisplay

--- gamma.py
+++ gamma.py
@@ -24,27 +24,7 @@ image = None
 """
 
 
-def gammaCorrection2(img: np.ndarray, gamma=1.0):
-    invGamma = 1.0 / gamma
-    table = np.array([(np.power((i / 255.0), invGamma)) * 255
-        for i in np.arange(0, 256)]).astype("uint8")
-    return cv2.LUT(img, table)
-    pass
-
-
-def gammaCorrection1(img: np.ndarray, gamma: int) -> np.ndarray:
-    intensity_transform = np.arange(256)
-    new_img = np.zeros(img.shape)
-    for i in range(1, 256):
-        intensity_transform[i] = i**(0.01/gamma)
-        new_img[img == i] = intensity_transform[i]
-    image = new_img
-    return new_img
-    pass
-
-
-def gammaCorrection(img: np.ndarray, gamma: int) -> np.ndarray:
-    gamma = 0.01 / gamma
+def gammaCorrection(img: np.ndarray, gamma: float) -> np.ndarray:
     g_array = np.full(img.shape, gamma)
     new_img = np.power(img, g_array)
     return new_img
@@ -58,40 +38,29 @@ def gammaDisplay(img_path: str, rep: int):
         img = cv2.imread(img_path)
     cv2.namedWindow("Gamma display")
     cv2.imshow("Gamma display", img)
-    global image
-    image = img / 255.0
-    cv2.createTrackbar("Gamma", "Gamma display", 1, 200, on_trackbar)
-    # on_trackbar(100)
-    cv2.waitKey()  # Wait until user press some key
-    """
-    while 1:
-        print("im hresadasdasdasr")
-
-        print("im hrer")
-        pos = cv2.getTrackbarPos("Gamma", "Gamma display")
-        print(pos)
-        new_img = gammaCorrection(img, pos)
+    cv2.createTrackbar("Gamma*100", "Gamma display", 1, 200, on_trackbar)
+    # cv2.resizeWindow("Gamma display", 700, 500)
+    while True:
+        pos = cv2.getTrackbarPos("Gamma*100", "Gamma display")
+        pos = pos / 200
+        new_img = gammaCorrection(img / 255.0, pos)
         cv2.imshow("Gamma display", new_img)
-        key = cv2.waitKey(1000)
-        if key == 27:
+        key = cv2.waitKey(1000)  # Wait until user press some key
+        if key == 27:  # Esc
             break
-        if cv2.getWindowsProperty("Gamma display", cv2.WND_PROP_VISIABLE) < 1:
+        if cv2.getWindowProperty("Gamma display", cv2.WND_PROP_VISIBLE) < 1:
             break
-        cv2.waitKey(0)  # close window when a key press is detected
-        cv2.destroyWindow('image')
-        cv2.waitKey(1)
-    """
+    cv2.destroyWindow('image')
     pass
 
 
-def on_trackbar(x: int):
-    new_img = gammaCorrection(image, x)
-    cv2.imshow("Gamma display", new_img)
+def on_trackbar(x: int):  # do nothing
     pass
 
 
 def main():
     gammaDisplay('bac_con.png', LOAD_GRAY_SCALE)
+    # gammaDisplay('beach.jpg', 2)
 
 
 if __name__ == '__main__':
